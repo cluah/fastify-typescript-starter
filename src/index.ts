f3b49a4c89a91d72e8094448bc15312e8fd2fce9
camilo <122648346+cluah@users.noreply.github.com>
2023-12-27 14:03:10 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import app from "./app";
+import fastify from 'fastify';
+import { Browser } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
+const server = fastify();
 
-app.listen({ port: FASTIFY_PORT });
+let browser: Browser | null = null;
 
-console.log(`🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`);
-console.log(`Route index: /`);
-console.log(`Route user: /api/v1/user`);
+server.get('/', async (_request, _reply) => {
+  if (!browser) return 'browser not ready';
+
+  const page = await browser.newPage();
+  await page.goto('https://www.diariooficial.interior.gob.cl/');
+
+  try {
+    await page.waitForSelector('#tramites', { timeout: 3000 });
+    const content = await page.content();
+    return content;
+  } catch (err) {
+    return ':(';
+  }
+});
+
+server.listen({ port: 8080 }, async (err, address) => {
+  browser = await puppeteer.use(StealthPlugin()).launch({ headless: 'new' });
+
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
